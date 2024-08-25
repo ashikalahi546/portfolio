0946314d8e5f0e21bcea9823692522aef6491598
ashikalahi546 <144088399+ashikalahi546@users.noreply.github.com>
2024-08-25 17:54:53 +0600

    update

--- src/components/navber/Navber.jsx
+++ src/components/navber/Navber.jsx
@@ -8,23 +8,23 @@ const Navbar = () => {
 
   return (
     <div className="text-white 2xl:w-[1440px] w-full mx-auto pt-7 xl:px-[120px] sm:px-5 flex items-center justify-between">
-      <Link href="/" className="text-4xl font-bold cursor-pointer sm:pl-0 pl-5">
+      <Link href="/" className="md:text-4xl text-2xl font-bold cursor-pointer sm:pl-0 pl-5">
         Ashik
       </Link>
 
       {/* Hamburger and Cross icons */}
       <div
         onClick={() => setOpen(!open)}
-        className="text-white md:hidden absolute right-[26px] z-10"
+        className="text-white md:hidden absolute right-[26px] "
       >
         {open ? <RxCross2 size={24} /> : <RxHamburgerMenu size={24} />}
       </div>
 
       {/* Navigation Menu */}
       <ul
-        className={`flex md:flex-row flex-col md:gap-y-0 gap-y-2 gap-x-10 md:text-lg absolute md:static top-20 md:top-0 left-0 md:w-auto w-full text-sm rounded p-5 md:p-0 bg-white md:bg-transparent text-black md:text-white 
-        ${open ? "translate-x-0 opacity-100" : "-translate-x-full opacity-0 pointer-events-none "} 
-        md:pointer-events-auto md:opacity-100 md:transform-none 
+        className={`flex md:flex-row flex-col md:gap-y-0 gap-y-2 gap-x-10 md:text-lg absolute md:static top-20 md:top-0  md:w-auto w-full text-sm rounded p-5 md:p-0  text-black md:text-white 
+        ${open ? "translate-x-0 opacity-100 bg-white md:bg-transparent left-0" : "-translate-x-full opacity-0 pointer-events-none -left-28"} 
+        md:pointer-events-auto md:opacity-100 opacity-0 md:transform-none 
         transition-all  duration-700 ease-in-out md:transition-none `}
       >
         <li>
